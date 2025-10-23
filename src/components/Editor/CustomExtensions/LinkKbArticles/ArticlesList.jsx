8136caeaf813622e7505f8e467ca56c200e01d6a
Joseph Mathew <josephmathew900@gmail.com>
2025-10-23 16:30:59 +0530

    Added keyboard navigation and fixed modal position of Link to NeetoKB article

--- src/components/Editor/CustomExtensions/LinkKbArticles/ArticlesList.jsx
+++ src/components/Editor/CustomExtensions/LinkKbArticles/ArticlesList.jsx
@@ -1,49 +1,47 @@
 import React from "react";
 
+import classNames from "classnames";
 import { isNotEmpty } from "neetocist";
 import { withT } from "neetocommons/react-utils";
 import { Typography as NeetoUITypography } from "neetoui";
-import { Trans } from "react-i18next";
 
 import { decodeHtmlEntities } from "src/utils/common";
 
 const ArticlesList = withT(
-  ({ t, articles = [], searchTerm = "", onSelectArticle }) => (
-    <>
-      <NeetoUITypography className="mb-3" style="h6" weight="semibold">
-        {searchTerm ? (
-          <Trans
-            i18nKey="neetoEditor.linkKb.articlesMatching"
-            values={{ entity: searchTerm }}
-          />
-        ) : (
-          t("neetoEditor.linkKb.selectArticle")
-        )}
-      </NeetoUITypography>
-      <div className="max-h-40 overflow-y-auto">
-        {isNotEmpty(articles) ? (
-          <div className="space-y-1">
-            {articles.map(article => (
-              <button
-                className="hover:neeto-ui-bg-gray-100 focus:neeto-ui-bg-gray-100 neeto-ui-rounded w-full p-2 text-left focus:outline-none"
-                key={article?.id}
-                onClick={() => onSelectArticle?.(article)}
-              >
-                <div className="neeto-ui-text-sm neeto-ui-font-medium">
-                  {decodeHtmlEntities(article?.title)}
-                </div>
-              </button>
-            ))}
-          </div>
-        ) : (
-          <div className="neeto-ui-text-gray-500 py-4 text-center">
-            <NeetoUITypography style="body2">
-              {t("neetoEditor.error.noResults")}
-            </NeetoUITypography>
-          </div>
-        )}
-      </div>
-    </>
+  ({
+    t,
+    articles = [],
+    onSelectArticle,
+    highlightedIndex = 0,
+    containerRef,
+  }) => (
+    <div className="max-h-40 overflow-y-auto" ref={containerRef}>
+      {isNotEmpty(articles) ? (
+        <div className="space-y-1">
+          {articles.map((article, index) => (
+            <button
+              data-index={index}
+              key={article?.id}
+              className={classNames(
+                "hover:neeto-ui-bg-gray-200 focus:neeto-ui-bg-gray-200 neeto-ui-rounded w-full p-2 text-left focus:outline-none",
+                { "neeto-ui-bg-gray-200": index === highlightedIndex }
+              )}
+              onClick={() => onSelectArticle?.(article)}
+            >
+              <div className="neeto-ui-text-sm neeto-ui-font-medium">
+                {decodeHtmlEntities(article?.title)}
+              </div>
+            </button>
+          ))}
+        </div>
+      ) : (
+        <div className="neeto-ui-text-gray-500 py-4 text-center">
+          <NeetoUITypography style="body2">
+            {t("neetoEditor.error.noResults")}
+          </NeetoUITypography>
+        </div>
+      )}
+    </div>
   )
 );
 
