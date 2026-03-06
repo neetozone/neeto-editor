e9f4478b750fc8e1624d8bb5284623e0de9a1001
Praveen Murali <itspraveenmurali@gmail.com>
2026-03-06 23:35:46 +0530

    Fix paragraph spacing inconsistency

--- src/components/Editor/utils.js
+++ src/components/Editor/utils.js
@@ -74,8 +74,7 @@ export const isEmojiSuggestionsMenuActive = () =>
   !!document.querySelector(".neeto-editor-emoji-suggestion");
 
 export const transformPastedHTML = content => {
-  const contentWithoutBr = content.replaceAll("<br />", "<p></p>");
-  const doc = new DOMParser().parseFromString(contentWithoutBr, "text/html");
+  const doc = new DOMParser().parseFromString(content, "text/html");
 
   doc.querySelectorAll("[style]").forEach(el => {
     el.style.color = "";
