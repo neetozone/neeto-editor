38145ce4473b44ead0c0ff933599bb24ffe281b9
Thomas Mathai <85351738+thomas-mathai@users.noreply.github.com>
2026-03-10 12:41:40 +0400

    Updated the highlight colors and Fixed the mapping (#1760)
    * Updated the light mode colors mapping
    
    * Updated the dark mode colors
    
    * Updated the color picker layout to organize colors by families and arrange shades from dark to light

--- src/components/Editor/Menu/Fixed/components/HighlightDropdown/ColorDot.jsx
+++ src/components/Editor/Menu/Fixed/components/HighlightDropdown/ColorDot.jsx
@@ -1,18 +1,12 @@
 import classNames from "classnames";
 import { hyphenize } from "neetocommons/utils";
-import { Typography } from "neetoui";
 
 const ColorDot = ({ colorVar, isSelected, onClick, isTextColor }) => {
   const dotClass = classNames("neeto-editor-highlight-dropdown__color-dot", {
     "neeto-editor-highlight-dropdown__color-dot--selected": isSelected,
-    "neeto-editor-highlight-dropdown__color-dot--background": !isTextColor,
   });
 
-  const dotStyle = {
-    ...(isTextColor
-      ? { color: `var(${colorVar})` }
-      : { backgroundColor: `var(${colorVar})` }),
-  };
+  const dotStyle = { backgroundColor: `var(${colorVar})` };
 
   const colorNumber = colorVar.match(/\d+$/)?.[0] || colorVar;
 
@@ -24,17 +18,7 @@ const ColorDot = ({ colorVar, isSelected, onClick, isTextColor }) => {
       data-testid={`neeto-editor-highlight-${hyphenize(
         isTextColor ? "text" : "background"
       )}-color-dot-${colorNumber}`}
-    >
-      {isTextColor && (
-        <Typography
-          className="neeto-editor-highlight-dropdown__color-dot-text"
-          style="body2"
-          weight="semibold"
-        >
-          A
-        </Typography>
-      )}
-    </div>
+    />
   );
 };
 
--- src/components/Editor/Menu/Fixed/components/HighlightDropdown/constants.js
+++ src/components/Editor/Menu/Fixed/components/HighlightDropdown/constants.js
@@ -1,27 +1,84 @@
 export const COLORS = {
   recent: ["--neeto-editor-highlight-bg-yellow-light"],
   text: [
+    // Neutrals
+    "--neeto-editor-highlight-text-11",
     "--neeto-editor-highlight-text-1",
     "--neeto-editor-highlight-text-2",
+    "--neeto-editor-highlight-text-12",
+    "--neeto-editor-highlight-text-13",
+    "--neeto-editor-highlight-text-14",
+    // Reds
+    "--neeto-editor-highlight-text-15",
+    "--neeto-editor-highlight-text-10",
+    "--neeto-editor-highlight-text-16",
+    // Oranges / Browns
     "--neeto-editor-highlight-text-3",
     "--neeto-editor-highlight-text-4",
+    // Yellows / Golds
     "--neeto-editor-highlight-text-5",
+    "--neeto-editor-highlight-text-18",
+    "--neeto-editor-highlight-text-17",
+    // Greens
     "--neeto-editor-highlight-text-6",
+    "--neeto-editor-highlight-text-19",
+    "--neeto-editor-highlight-text-21",
+    // Teals
+    "--neeto-editor-highlight-text-20",
+    "--neeto-editor-highlight-text-24",
+    // Blues
+    "--neeto-editor-highlight-text-23",
     "--neeto-editor-highlight-text-7",
+    "--neeto-editor-highlight-text-22",
+    "--neeto-editor-highlight-text-25",
+    // Purples
+    "--neeto-editor-highlight-text-27",
     "--neeto-editor-highlight-text-8",
+    "--neeto-editor-highlight-text-29",
+    // Pinks / Magentas
+    "--neeto-editor-highlight-text-28",
+    "--neeto-editor-highlight-text-26",
     "--neeto-editor-highlight-text-9",
-    "--neeto-editor-highlight-text-10",
   ],
   background: [
-    "--neeto-editor-highlight-bg-1",
-    "--neeto-editor-highlight-bg-2",
+    // Neutrals
+    "--neeto-editor-highlight-bg-11",
+    "--neeto-editor-highlight-bg-12",
+    "--neeto-editor-highlight-bg-13",
+    "--neeto-editor-highlight-bg-14",
     "--neeto-editor-highlight-bg-3",
-    "--neeto-editor-highlight-bg-4",
-    "--neeto-editor-highlight-bg-5",
-    "--neeto-editor-highlight-bg-6",
-    "--neeto-editor-highlight-bg-7",
-    "--neeto-editor-highlight-bg-8",
-    "--neeto-editor-highlight-bg-9",
+    "--neeto-editor-highlight-bg-2",
+    "--neeto-editor-highlight-bg-15",
+    // Reds
+    "--neeto-editor-highlight-bg-16",
+    "--neeto-editor-highlight-bg-17",
     "--neeto-editor-highlight-bg-10",
+    // Oranges / Browns
+    "--neeto-editor-highlight-bg-18",
+    "--neeto-editor-highlight-bg-4",
+    // Yellows / Golds
+    "--neeto-editor-highlight-bg-19",
+    "--neeto-editor-highlight-bg-1",
+    "--neeto-editor-highlight-bg-5",
+    // Greens
+    "--neeto-editor-highlight-bg-20",
+    "--neeto-editor-highlight-bg-6",
+    // Teals
+    "--neeto-editor-highlight-bg-21",
+    "--neeto-editor-highlight-bg-24",
+    // Blues
+    "--neeto-editor-highlight-bg-23",
+    "--neeto-editor-highlight-bg-22",
+    "--neeto-editor-highlight-bg-25",
+    "--neeto-editor-highlight-bg-7",
+    // Purples
+    "--neeto-editor-highlight-bg-28",
+    "--neeto-editor-highlight-bg-30",
+    "--neeto-editor-highlight-bg-8",
+    // Pinks / Magentas
+    "--neeto-editor-highlight-bg-29",
+    "--neeto-editor-highlight-bg-26",
+    "--neeto-editor-highlight-bg-27",
+    "--neeto-editor-highlight-bg-9",
   ],
 };
